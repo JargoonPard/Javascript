0e41691e4745ef95203d4627136113fdd474c613
Larry Guger <larrygug@microsoft.com>
2017-09-14 10:37:20 -0700

    Add a page to test some f12 memory tool features

---
+++ Memory/memoryConsumer.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let multiplier = 1;
+
+function init(){
+    const addPicButton = document.getElementById("useMemoryButton");
+    addPicButton.addEventListener("click", AddAnotherPicture);
+}
+
+function AddAnotherPicture() {
+    const picturesDiv = document.getElementById('pictures');
+
+    let newImage = document.createElement("img");
+    newImage.src = "GolfShoes.jpg";
+    newImage.height = 100;
+    newImage.width = 100;
+
+    picturesDiv.appendChild(newImage);
+
+    if(picturesDiv.children.length > (10 * multiplier))
+    {
+        //place a performance marker every 10 children
+        performance.mark(`${10 * multiplier} mch (more children here)`);
+        multiplier++;
+    }
+}
--- Promises/nestingPromises.js
+++ Promises/nestingPromises.js
@@ -3,54 +3,58 @@ var promiseCount = 0;
 
 console.log(`Line 4 promiseCount is: ${promiseCount}`);
 goDoSomeWork().then(val => {
-            console.log(`Line 6, val is:${val}. Promise fulfilled (Async code terminated)`);
-        }).catch(reason => {
-            console.log(`Line 8. Handle rejected promise (${reason}) here.`);
-        });
+    console.log(`Line 6, val is:${val}. Promise fulfilled (Async code terminated)`);
+}).catch(reason => {
+    console.log(`Line 8. Handle rejected promise (${reason}) here.`);
+});
 
 console.log(`Line 11 promiseCount is: ${promiseCount}`);
 
 goDoSomeWork().then(val => {
-            console.log(`Line 14, val is:${val}. Promise fulfilled (Async code terminated)`);
-        }).catch(reason => {
-            console.log(`Line 16. Handle rejected promise (${reason}) here.`);
-        });
+    console.log(`Line 14, val is:${val}. Promise fulfilled (Async code terminated)`);
+}).catch(reason => {
+    console.log(`Line 16. Handle rejected promise (${reason}) here.`);
+});
 
 console.log(`Line 19 promiseCount is: ${promiseCount}`);
 
 goDoSomeWork().then(val => {
-            console.log(`Line 22, val is:${val}. Promise fulfilled (Async code terminated)`);
-        }).catch(reason => {
-            console.log(`Line 24. Handle rejected promise (${reason}) here.`);
-        });
+    console.log(`Line 22, val is:${val}. Promise fulfilled (Async code terminated)`);
+}).catch(reason => {
+    console.log(`Line 24. Handle rejected promise (${reason}) here.`);
+});
+
+console.log(`Line 27. Done.`);
 
 function goDoSomeWork() {
     var thisPromiseCount = ++promiseCount;
 
-    console.log(`Line 30, thisPromiseCount is:${thisPromiseCount}. Started (Sync code started)`);
+    console.log(`Line 32, thisPromiseCount is:${thisPromiseCount}. Started (Sync code started)`);
 
     // We make a new promise: we promise a numeric count of this promise, starting from 1 (after waiting 3s)
     return new Promise((resolve, reject) => {
-            console.log(`Line 34, thisPromiseCount is:${thisPromiseCount}. Promise started (Async code started)`);
-            // This is only an example to create asynchronism
-            setTimeout(() => {NestOne(thisPromiseCount).then(console.log(`Line 36.`))}, Math.random()*10000);
+        console.log(`Line 36, thisPromiseCount is:${thisPromiseCount}. Promise started (Async code started)`);
+        // This is only an example to create asynchronism
+        setTimeout(() => { NestOne(thisPromiseCount).then(console.log(`Line 38.`)) }, Math.random() * 10000);
 
-            console.log(`Line 42, call resolve after the setTimeout function.`);
-            resolve(thisPromiseCount);
-        }
-    );
+        console.log(`Line 40, call resolve after the setTimeout function.`);
+        resolve(thisPromiseCount);
+    }
+    ).then(val => {
+        console.log(`Line 44, val is:${val}. Promise fulfilled (Async code terminated)`);
+    });
 }
 
 function NestOne(input) {
     return new Promise((resolve, reject) => {
-        console.log(`Line 46. NestOne, input: ${input}`);
-        setTimeout(()=>{NestTwo(input).then(()=>{console.log(`Line 47.`)}), Math.random()*10000});
+        console.log(`Line 50. NestOne, input: ${input}`);
+        setTimeout(() => { NestTwo(input).then(() => { console.log(`Line 47.`) }), Math.random() * 10000 });
     });
 }
 
 function NestTwo(input) {
     return new Promise((resolve, reject) => {
-        console.log(`Line 53. NestTwo, input: ${input}`);
-        setTimeout(()=>{console.log("Line 54 NestTwo timeout"), Math.random()*10000});
+        console.log(`Line 57. NestTwo, input: ${input}`);
+        setTimeout(() => { console.log("Line 58 NestTwo timeout"), Math.random() * 10000 });
     });
 }
